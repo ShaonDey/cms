33fb93923c75774d815c22a78fa8c6509a22c609
Shaon Dey <shaond3y@gmail.com>
2020-09-30 01:23:55 +0600

    Add application properties to configure DB to store and persist data, Remove commented out codes from customer service

--- src/main/java/com/shaondey/cms/service/CustomerService.java
+++ src/main/java/com/shaondey/cms/service/CustomerService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
 public class CustomerService {
@@ -14,47 +13,24 @@ public class CustomerService {
     @Autowired
     private CustomerDAO customerDAO;
 
-    private int nextCustomerIdCount = 1;
-    private List<Customer> customerList = new CopyOnWriteArrayList<>();
-
     public Customer addCustomer(Customer customer) {
-//        customer.setCustomerId(nextCustomerIdCount);
-//        customerList.add(customer);
-//        nextCustomerIdCount++;
-//        return customer;
         return customerDAO.save(customer);
     }
 
     public List<Customer> getCustomers() {
-//        return customerList;
         return customerDAO.findAll();
     }
 
     public Customer getCustomer(int customerId) {
-//        return customerList
-//                .stream()
-//                .filter(c -> c.getCustomerId() == customerId)
-//                .findFirst()
-//                .get();
         return customerDAO.findById(customerId).get();
     }
 
     public Customer updateCustomer(int customerId, Customer customer) {
-//        customerList.stream().forEach(c -> {
-//            if (c.getCustomerId() == customerId) {
-//                c.setCustomerFirstName(customer.getCustomerFirstName());
-//                c.setCustomerLastName(customer.getCustomerLastName());
-//                c.setCustomerEmail(customer.getCustomerEmail());
-//            }
-//        });
-//
-//        return getCustomer(customerId);
         customer.setCustomerId(customerId);
         return customerDAO.save(customer);
     }
 
     public void deleteCustomer(int customerId) {
-//        customerList.removeIf(c -> c.getCustomerId() == customerId);
         customerDAO.deleteById(customerId);
     }
 }
