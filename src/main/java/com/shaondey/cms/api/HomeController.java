0f61103eebddc82217c3a4e117daf0aa1da1b4bd
Shaon Dey <shaond3y@gmail.com>
2020-09-29 17:09:19 +0600

    Add base path for all endpoints

--- src/main/java/com/shaondey/cms/api/HomeController.java
+++ src/main/java/com/shaondey/cms/api/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "api")
 public class HomeController {
 
     @RequestMapping(method = RequestMethod.GET, value = "index")
