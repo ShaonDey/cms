1f73b3bcaee107ce5bbc8b687a9c3398180c1edb
Shaon Dey <shaond3y@gmail.com>
2020-09-30 22:55:04 +0600

    Rename personal filter

--- src/main/java/com/shaondey/cms/filter/Filter_1.java
+++ src/main/java/com/shaondey/cms/filter/Filter_1.java
@@ -6,13 +6,13 @@ import javax.servlet.*;
 import java.io.IOException;
 
 @Service
-public class PersonalFilter implements Filter {
+public class Filter_1 implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("Personal Filter is called before endpoint execution");
+        System.out.println("Filter 1 is called before endpoint execution");
         chain.doFilter(request, response);
-        System.out.println("Personal Filter is called after endpoint execution but before returning response");
+        System.out.println("Filter 2 is called after endpoint execution but before returning response");
     }
 
 }
