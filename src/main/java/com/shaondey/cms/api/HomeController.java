491a5e3b11ea687cc250de03cabc7c27c29d5946
Shaon Dey <shaond3y@gmail.com>
2020-09-30 22:26:26 +0600

    Add current date time with hello world on home

--- src/main/java/com/shaondey/cms/api/HomeController.java
+++ src/main/java/com/shaondey/cms/api/HomeController.java
@@ -1,14 +1,16 @@
 package com.shaondey.cms.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 public class HomeController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "index")
+    @GetMapping
     public String conveyGreeting() {
-        return "Hello World";
+        return "Hello World! The time is now: " + new Date();
     }
+
 }
