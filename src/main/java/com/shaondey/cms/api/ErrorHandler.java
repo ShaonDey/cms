af4ca7e8b2e648d1a269e401ddd1ae7397e7ebd4
Shaon Dey <shaond3y@gmail.com>
2020-09-30 22:05:04 +0600

    Add custom exception handler for customer not found

---
+++ src/main/java/com/shaondey/cms/api/ErrorHandler.java
@@ -0,0 +1,29 @@
+package com.shaondey.cms.api;
+
+import com.shaondey.cms.exception.ApplicationError;
+import com.shaondey.cms.exception.CustomerNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class ErrorHandler extends ResponseEntityExceptionHandler {
+
+    @Autowired
+    private ApplicationError applicationError;
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<ApplicationError> handleCustomerNotFoundException(CustomerNotFoundException exception, WebRequest webRequest) {
+        applicationError.setCode(101);
+        applicationError.setMessage(exception.getMessage());
+
+        return new ResponseEntity<>(applicationError, HttpStatus.NOT_FOUND);
+    }
+
+}
